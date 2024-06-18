2cd01f9019462766d9eb5467601500fab65cd566
kopo <kopo@DESKTOP-H5TPRSM>
2024-06-18 19:16:33 +0900

    결과값
    a[][] 배열 :
    [1, 10, 3, 4]
    [5, 6, 7]
    
    b[][] 배열 :
    [1, 10, 3, 4]
    [5, 6, 7]
    
    c[] 배열 :
    10
    2
    3
    4
    
    d[] 배열 :
    1
    2
    3
    4

--- Do_it/src/Ch02/CloneMethod.java
+++ Do_it/src/Ch02/CloneMethod.java
@@ -5,20 +5,31 @@ import java.util.Arrays;
 public class CloneMethod {
 
 	public static void main(String[] args) {
-
 		int[][] a = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
 		int[][] b = a.clone();
-		
 		a[0][1] = 10;
-		
+		System.out.println("a[][] 배열 : ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(Arrays.toString(a[i]));
+		}
+		System.out.println("\nb[][] 배열 : ");
 		for (int i = 0; i < b.length; i++) {
 			System.out.println(Arrays.toString(b[i]));
 		}
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(Arrays.toString(a[i]));
+
+		int[] c = { 1, 2, 3, 4 };
+		int[] d = c.clone();
+
+		c[0] = 10;
+		System.out.println("\nc[] 배열 : ");
+		for (int i = 0; i < c.length; i++) {
+			System.out.println(c[i]);
+		}
+		System.out.println("\nd[] 배열 : ");
+		for (int i = 0; i < d.length; i++) {
+			System.out.println(d[i]);
 		}
 
 	}
 
 }
-
